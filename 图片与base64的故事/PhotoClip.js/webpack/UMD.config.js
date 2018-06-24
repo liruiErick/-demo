928f4f4159865197ba7bfe1285c6deb6ae46c912
lirui <liruiruilitwo@163.com>
2018-06-24 15:26:22 +0800

    图片相关操作

---
+++ 图片与base64的故事/PhotoClip.js/webpack/UMD.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    'hammerjs': {
+        commonjs: 'hammerjs',
+        commonjs2: 'hammerjs',
+        amd: 'hammerjs',
+        root: 'Hammer'
+    },
+    'iscroll/build/iscroll-zoom': {
+        commonjs: 'iscroll/build/iscroll-zoom',
+        commonjs2: 'iscroll/build/iscroll-zoom',
+        amd: 'iscroll',
+        root: 'IScroll'
+    },
+    'lrz': 'lrz'
+};
